44d95589f4f23cbe123582689314d2f18d6140aa
Nader Elasmar <smirkwoodia@gmail.com>
2022-06-26 00:41:21 -0500

    CSS work on Profile Management
    Added CSS and requirements to the text boxes in profile management

--- src/pages/ProfileManagement.js
+++ src/pages/ProfileManagement.js
@@ -10,11 +10,11 @@ function ProfileManagement(){
                     <label>
                         <div class="input">                            
                             Full Name:
-                            <input type="text" name="FullName" maxLength = "50"/><br></br>
+                            <input type="text" name="FullName" maxLength = "50" required/><br></br>
                         </div>
                         <div class="input">
                             Address 1:
-                            <input type="text" name="Address1" maxLength = "100"/><br></br>
+                            <input type="text" name="Address1" maxLength = "100" required/><br></br>
                         </div>
 						<div class="input">
                             Address 2:
@@ -22,24 +22,26 @@ function ProfileManagement(){
                         </div>
 						<div class="input">
                             City:
-                            <input type="text" name="City" maxLength = "100"/><br></br>
+                            <input type="text" name="City" maxLength = "100" required/><br></br>
+                        </div>
+                        <div class="input">
+                            Five digit zip code:
+                            <input type="text" name="ZipCode" pattern="[0-9]{5,}" required/><br></br>
                         </div>
 						<div class="input">
+                        Select State
                             <form>
-                                <label>
-                                    Select State
-                                </label>
-                                <select>
+                                <select class="box" required>
                                     <option value = "DBcodes">DB will store these 2 character codes</option>
                                 </select>
                             </form>
                         </div>
-                        <div class="input">
-                            Zip Code:
-                            <input type="text" name="ZipCode"/><br></br>
+                        <div>
+                            <br></br>
+                        <input type="submit" value="Save"/> 
                         </div>
-                    </label>                    
-                    <input type="submit" value="Save"/><br></br>
+                    </label>      
+                     
                 </form>
             </div>
         </div>
