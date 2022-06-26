493e9bf7db79dade79d42969cc30d573820f0414
Nader Elasmar <smirkwoodia@gmail.com>
2022-06-26 01:03:39 -0500

    Update PricingModule.js
    Added Total Amount Due and text field

--- src/pages/PricingModule.js
+++ src/pages/PricingModule.js
@@ -4,6 +4,8 @@ function PricingModule(){
 	return(
 			<div classname="PricingModule">
 				<h1>Pricing Module: Under Construction</h1>
+				Total Amount Due: 
+				<input type="text" value="numeric non-editable, calculated (gallons * price)" disable/>
 			</div>
 	);
 }
