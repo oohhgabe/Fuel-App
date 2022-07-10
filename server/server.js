0b4240a6e4067e244154c721f750a1fdf0a28900
skmz99 <83142392+skmz99@users.noreply.github.com>
2022-07-10 15:58:14 -0500

    express works with form and history

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const app = express();
 app.use(express.static('public'))
+////////// Below (lines 5,6,7 does similar things as lines 9,10 keeping just in case) ////////
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+///////// above (lines 5,6,7 does similar things as lines 9,10 keeping just in case)////////
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.listen(5000, () => console.log("server created in 5000"))
 
+let info = [];
 
 app.all('/*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -15,16 +20,23 @@ app.all('/*', function (req, res, next) {
     next();
   });
 
+app.get('/api', (req,res) => {
+    res.json({info})
+})
+
 app.post('/create', (req,res)=>{
     console.log("Recieved New Data");
-    console.log(req.body);
-    const data = req.body;
+    var newInfo = {
+        Gal_Req: req.body.data.Gal_Req, //req.body is to access the whole body information and req.body.(from file where you saved your states) to access specific values
+        Del_Add: req.body.data.Del_Add,
+        Del_Dat: req.body.data.Del_Dat,
+        Sug_Pri: req.body.data.Sug_Pri,
+        Tot_Amo: req.body.data.Tot_Amo
+    }
+    info.push(newInfo);
+    console.log(info);
     res.json({
         status: "success",
-        Gal_Req: data.Gal_Req,
-        Del_Add: data.Del_Add,
-        Del_Dat: data.Del_Dat,
-        Sug_Pri: data.Sug_Pri,
-        Tot_Amo: data.Tot_Amo
+        Data: req.body
     });
 });
--- src/pages/FuelQuoteHistory.js
+++ src/pages/FuelQuoteHistory.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from './FuelQuote.module.css';
 
-//data to text out the table
-const data=[
-    {date: "12/12/1212", gallons: 100, suggested: 200, total: 30000},
-    {date: "14/14/1414", gallons: 500, suggested: 600, total: 70000},
-    {date: "18/18/1818", gallons: 900, suggested: 1000, total: 1100000},
-]
+
 
 function FuelQuoteHistory(){
+    const [backendData, setBackendData] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api')
+        .then(res => {
+            return res.json();
+        })
+        .then( data => {
+            setBackendData(data.info)
+        })
+    },[]);
+   
     return(
         <div className="FuelQuoteHistory">
             <div className={styles.table_box}>
                 <table className={styles.table}>
-                    <tr>
+                <tr>
                         <th className={styles.th}>Deliver Date</th>
                         <th className={styles.th}>Gallons Requested</th>
                         <th className={styles.th}>Suggested Price/Gallons</th>
                         <th className={styles.th}>Total Amount</th>
                     </tr>
-                    {data.map((val,key) => {
-                        return(
-                            <tr key={key}>
-                                <td className={styles.td}>{val.date}</td>
-                                <td className={styles.td}>{val.gallons}</td>
-                                <td className={styles.td}>{val.suggested}</td>
-                                <td className={styles.td}>{val.total}</td>
-                            </tr>
-                        )
-                    })}
-                </table>
+                {backendData.map((val,key) =>{
+                    return(
+                        <tr key={val.key}>
+                            <th className={styles.th}>{val.Del_Dat}</th>
+                            <th className={styles.th}>{val.Gal_Req}</th>
+                            <th className={styles.th}>{val.Sug_Pri}</th>
+                            <th className={styles.th}>{val.Tot_Amo}</th>
+                        </tr>
+                    );
+                })}
+            </table>
             </div>
         </div>
     );
