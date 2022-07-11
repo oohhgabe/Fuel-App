c0da124a43f714b030a996adf9f6b2c9d271285d
Nader Elasmar <smirkwoodia@gmail.com>
2022-07-10 23:41:09 -0500

    Update PricingModule.js

--- src/pages/PricingModule.js
+++ src/pages/PricingModule.js
@@ -1,5 +1,26 @@
 import React from "react";
 
+class priceModule {
+	constructor(location, RateHistory, GallonsRequested) {
+		if (location == "TX") {
+			this.locationFactor = .02;
+		}
+		else {
+			this.locationFactor = .04
+		}
+		this.RateHistoryFactor = RateHistory;
+		if (GallonsRequested > 1000) {
+			this.GallonsRequestedFactor = .02
+		} else {
+			this.GallonsRequestedFactor = .03
+		}
+		this.margin = 1.5 * (this.locationFactor - this.RateHistoryFactor + this.GallonsRequestedFactor + .1)
+		this.suggestedPrice = this.margin + 1.5
+		this.totalDue = this.suggestedPrice * this.GallonsRequested
+	}
+	
+}
+
 function PricingModule(){
 	return(
 			<div classname="PricingModule">
